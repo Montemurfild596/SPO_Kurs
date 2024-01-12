362e1525ef75c3727f1a81556e457d90ac8e3af2
Montemurfild596 <88890360+Montemurfild596@users.noreply.github.com>
2024-01-12 23:58:26 +0500

    Create client.c

---
+++ client.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024
+
+pthread_mutex_t *mutex;
+int *shared_sum;
+
+void cleanup() {
+    shmdt(mutex);
+}
+
+int main() {
+    key_t key = ftok("/tmp", 'A');
+    int shmid;
+
+    if ((shmid = shmget(key, sizeof(pthread_mutex_t) + sizeof(int), 0666)) < 0) {
+        perror("shmget failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((mutex = (pthread_mutex_t *)shmat(shmid, NULL, 0)) == (pthread_mutex_t *)-1) {
+        perror("shmat failed");
+        exit(EXIT_FAILURE);
+    }
+
+    shared_sum = (int *)(mutex + 1);
+
+    while (1) {
+        pthread_mutex_lock(mutex);
+
+        // Ввод числа с клавиатуры
+        int number;
+        printf("Client: Enter a number (0 to exit): ");
+        scanf("%d", &number);
+
+        // Отправляем число серверу
+        printf("Client: Sending number %d to server\n", number);
+
+        pthread_mutex_unlock(mutex);
+
+        // Проверяем, было ли введено число 0 (сигнал завершения)
+        if (number == 0) {
+            break;
+        }
+
+        // Пауза для предотвращения слишком частого обновления
+        sleep(1);
+    }
+
+    atexit(cleanup);
+
+    return 0;
+}
