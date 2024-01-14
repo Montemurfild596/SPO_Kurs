1c9c2084b3540c01df596a170be9f6d05ce74ee9
Montemurfild596 <88890360+Montemurfild596@users.noreply.github.com>
2024-01-15 01:39:10 +0500

    Update client_2.c

--- client_2.c
+++ client_2.c
@@ -1,24 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <sys/sem.h>
 
-#define SHM_SIZE 1024
+#define SHM_KEY 123456
+#define SEM_KEY 654321
 
 int main() {
-    key_t key = ftok("shmfile", 65);
-    int shmid = shmget(key, SHM_SIZE, 0666|IPC_CREAT);
-    int *shared_memory = (int*)shmat(shmid, NULL, 0);
+    int shmid, semid;
+    int *shared_memory;
+    struct sembuf sem_op;
 
-    while (1) {
-        int num;
-        printf("Enter a number: ");
-        scanf("%d", &num);
-        *shared_memory = num;
-        printf("Sum: %d\n", *shared_memory);
+    // Получаем доступ к разделяемой памяти
+    if ((shmid = shmget(SHM_KEY, sizeof(int), 0666)) < 0) {
+        perror("shmget");
+        exit(1);
     }
 
-    shmdt(shared_memory);
+    // Получаем доступ к разделяемой памяти
+    if ((shared_memory = shmat(shmid, NULL, 0)) == (int *) -1) {
+        perror("shmat");
+        exit(1);
+    }
+
+    // Получаем доступ к семафору
+    if ((semid = semget(SEM_KEY, 1, 0666)) < 0) {
+        perror("semget");
+        exit(1);
+    }
+
+    // Вводим число
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    // Записываем число в разделяемую память
+    sem_op.sem_num = 0;
+    sem_op.sem_op = -1;
+    sem_op.sem_flg = 0;
+    semop(semid, &sem_op, 1);
+    *shared_memory = number;
+    sem_op.sem_op = 1;
+    semop(semid, &sem_op, 1);
+
+    // Освобождаем разделяемую память
+    if (shmdt(shared_memory) < 0) {
+        perror("shmdt");
+        exit(1);
+    }
+
     return 0;
 }
