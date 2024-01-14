2115d553461e9db72a74a4c6d04e27574f1f0515
Montemurfild596 <88890360+Montemurfild596@users.noreply.github.com>
2024-01-15 03:25:34 +0500

    Update client_2.c

--- client_2.c
+++ client_2.c
@@ -1,54 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <semaphore.h>
 
-#define SHM_SIZE 1024
-
-int *shared_memory;
-sem_t *semaphore;
+#define SHM_SIZE  sizeof(int)
 
 int main() {
-    // Получение доступа к семафору
-    semaphore = sem_open("/my_sem", 0);
-    if (semaphore == SEM_FAILED) {
-        perror("sem_open");
-        exit(EXIT_FAILURE);
-    }
+    key_t key = ftok("shared_memory_key", 65);
+    int shmid = shmget(key, SHM_SIZE, 0666);
+    int *shared_data = (int *)shmat(shmid, NULL, 0);
 
-    // Получение доступа к общей памяти
-    key_t key = ftok("/tmp", 'S');
-    int shmid = shmget(key, SHM_SIZE * sizeof(int), 0666);
-    if (shmid < 0) {
-        perror("shmget");
-        exit(EXIT_FAILURE);
-    }
-
-    // Подключение общей памяти
-    shared_memory = (int*)shmat(shmid, NULL, 0);
-
-    // Запрос данных у пользователя
-    int numbers[SHM_SIZE];
-    printf("Клиент: Введите %d чисел:\n", SHM_SIZE);
-    for (int i = 0; i < SHM_SIZE; ++i) {
-        scanf("%d", &numbers[i]);
-    }
+    // Запрос у пользователя
+    int user_input;
+    printf("Введите число: ");
+    scanf("%d", &user_input);
 
     // Отправка запроса серверу
-    sem_wait(semaphore);
-    for (int i = 0; i < SHM_SIZE; ++i) {
-        shared_memory[i] = numbers[i];
-    }
-    sem_post(semaphore);
+    *shared_data = user_input;
 
     // Ожидание ответа от сервера
-    sleep(1);
+    sleep(1); // Пауза для синхронизации
 
-    // Отключение от общей памяти
-    shmdt(shared_memory);
+    // Вывод ответа
+    printf("Ответ от сервера: %d\n", *shared_data);
+
+    // Освобождение ресурсов
+    shmdt(shared_data);
 
     return 0;
 }
