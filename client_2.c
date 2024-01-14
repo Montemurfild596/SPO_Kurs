e74145ee5498163bcad5200aa42c732afc284f61
Montemurfild596 <88890360+Montemurfild596@users.noreply.github.com>
2024-01-15 01:35:34 +0500

    Update client_2.c

--- client_2.c
+++ client_2.c
@@ -3,56 +3,22 @@
 #include <string.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/types.h>
-#include <unistd.h>
 
-#define SHM_SIZE sizeof(int)
+#define SHM_SIZE 1024
 
 int main() {
-    int shm_id;
-    int *shm_ptr;
-    int num;
+    key_t key = ftok("shmfile", 65);
+    int shmid = shmget(key, SHM_SIZE, 0666|IPC_CREAT);
+    int *shared_memory = (int*)shmat(shmid, NULL, 0);
 
-    // Создаем разделяемую память
-    shm_id = shmget(IPC_PRIVATE, SHM_SIZE, IPC_CREAT | 0666);
-    if (shm_id == -1) {
-        perror("shmget");
-        exit(1);
+    while (1) {
+        int num;
+        printf("Enter a number: ");
+        scanf("%d", &num);
+        *shared_memory = num;
+        printf("Sum: %d\n", *shared_memory);
     }
 
-    // Получаем указатель на разделяемую память
-    shm_ptr = shmat(shm_id, NULL, 0);
-    if (shm_ptr == (int*)-1) {
-        perror("shmat");
-        exit(1);
-    }
-
-    // Читаем число от пользователя
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Записываем число в разделяемую память
-    *shm_ptr = num;
-
-    // Ждем, пока сервер не запишет сумму всех чисел в разделяемую память
-    while (*shm_ptr == num) {
-        sleep(1);
-    }
-
-    // Читаем сумму всех чисел из разделяемой памяти
-    printf("Sum of all numbers: %d\n", *shm_ptr);
-
-    // Отсоединяемся от разделяемой памяти
-    if (shmdt(shm_ptr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    // Удаляем разделяемую память
-    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
-        perror("shmctl");
-        exit(1);
-    }
-
+    shmdt(shared_memory);
     return 0;
 }
