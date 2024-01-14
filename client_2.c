28b3ac1f8f090cb45b7b2dbeb76c04b45de48e5e
Montemurfild596 <88890360+Montemurfild596@users.noreply.github.com>
2024-01-15 01:57:30 +0500

    Update client_2.c

--- client_2.c
+++ client_2.c
@@ -2,53 +2,44 @@
 #include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <sys/sem.h>
+#include <semaphore.h>
 
-#define SHM_KEY 123456
-#define SEM_KEY 654321
+#define SHM_SIZE  sizeof(int)
+
+int *shared_memory;
+sem_t *sem;
 
 int main() {
-    int shmid, semid;
-    int *shared_memory;
-    struct sembuf sem_op;
+    key_t key = ftok("shared_memory_key", 'R');
+    int shmid = shmget(key, SHM_SIZE, 0666);
+    shared_memory = (int *)shmat(shmid, NULL, 0);
 
-    // Получаем доступ к разделяемой памяти
-    if ((shmid = shmget(SHM_KEY, sizeof(int), 0666)) < 0) {
-        perror("shmget");
-        exit(1);
+    sem = sem_open("/my_semaphore", 0);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
     }
 
-    // Получаем доступ к разделяемой памяти
-    if ((shared_memory = shmat(shmid, NULL, 0)) == (int *) -1) {
-        perror("shmat");
-        exit(1);
+    int client_input;
+    while (1) {
+        printf("Enter a number: ");
+        scanf("%d", &client_input);
+
+        sem_wait(sem);  // Ждем, пока семафор не станет доступным
+
+        // Отправка числа на сервер
+        *shared_memory = client_input;
+
+        // Получение результата от сервера
+        printf("Client received result from server: %d\n", *shared_memory);
+
+        sem_post(sem);  // Освобождаем семафор
+
+        sleep(1);  // Имитация обработки результата
     }
 
-    // Получаем доступ к семафору
-    if ((semid = semget(SEM_KEY, 1, 0666)) < 0) {
-        perror("semget");
-        exit(1);
-    }
-
-    // Вводим число
-    int number;
-    printf("Enter a number: ");
-    scanf("%d", &number);
-
-    // Записываем число в разделяемую память
-    sem_op.sem_num = 0;
-    sem_op.sem_op = -1;
-    sem_op.sem_flg = 0;
-    semop(semid, &sem_op, 1);
-    *shared_memory = number;
-    sem_op.sem_op = 1;
-    semop(semid, &sem_op, 1);
-
-    // Освобождаем разделяемую память
-    if (shmdt(shared_memory) < 0) {
-        perror("shmdt");
-        exit(1);
-    }
+    sem_close(sem);
+    shmdt(shared_memory);
 
     return 0;
 }
